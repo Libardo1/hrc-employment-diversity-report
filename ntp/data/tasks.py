620098d51ab1af6906b25f03dd346c102960347e
Jon Staples <jstaples@nextgxdx.com>
2016-04-14 20:10:15 -0500

    feature(task): Created celery task
    The celery task simply wraps data.main.run() and can be run on a
    schedule.
    
    None

--- ntp/data/tasks.py
+++ ntp/data/tasks.py
@@ -2,6 +2,7 @@ import requests
 import time
 from celery import Celery
 import rethinkdb as r
+from ntp.data import main
 
 APP = Celery("ntp.data.tasks")
 APP.config_from_object("ntp.data.celeryconfig")
@@ -11,7 +12,7 @@ def check_and_fetch():
     """
     Check for an update to the HR data on the open data portal.  If one exists do.
     """
-    pass
+    main.run()
     
 @APP.task
 def test_task(a):
